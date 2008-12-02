8fb94d3057fc8969575b45600282b2503ac6ec3c
Vojtech Salbaba <darktatka@gmail.com>
2008-12-02 23:36:10 +0100

    prace na pingu, nefunguje

--- site-ping/newmain.cpp
+++ site-ping/newmain.cpp
@@ -20,36 +20,31 @@
 #include <netinet/in.h>
 #include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
-
+#include <iostream>
 #include <netdb.h>
 
 #define BUFSIZE 1500
-
+using namespace std;
 int finish = 0;
 
 /* Funkce checksum je opsaná z příslušných RFC dokumentů. */
-uint16_t checksum(uint16_t *addr, unsigned count)
-{
+unsigned short int checksum(unsigned short int *addr, unsigned count) {
 
-  uint32_t sum = 0;
-  while (count > 1)  {
-    sum += *(addr++);
-    count -= 2;
-  }
+    unsigned int sum = 0;
+    while (count > 1) {
+        sum += *(addr++);
+        count -= 2;
+    }
 
-  // mop up an odd byte, if necessary
-  if (count > 0) {
-    sum += *(unsigned char *)addr;
-  }
+    if (count > 0) {
+        sum += *(unsigned char *) addr;
+    }
 
-  // add back carry outs from top 16 bits to low 16 bits
-   while (sum >> 16)
+    while (sum >> 16)
         sum = (sum & 0xffff) + (sum >> 16);
 
-  return ~sum; // truncate to 16 bits
+    return ~sum; // truncate to 16 bits
 }
-
-
 
 void sig_handler(int sig) {
     if (sig == SIGINT)
@@ -58,18 +53,24 @@ void sig_handler(int sig) {
 
 int main(int argc, char *argv[]) {
     hostent *host;
-    host = gethostbyname(argv[1]);
+
     struct sigaction act;
-    struct icmphdr my_icmp_header;
+    struct icmphdr my_icmp_header, icmp_header;
     int sock;
+    size_t size;
     unsigned int ttl = 255;
-    timeval my_tv;
-    sockaddr_in my_sockaddr_in;
+    struct iphdr *ip_header;
+    struct timeval start, end, my_tv, timeout;
+    struct sockaddr_in my_sockaddr_in, received_address;
+    fd_set myset;
+    char buffer[BUFSIZE];
+
     memset(&act, 0, sizeof (act));
     act.sa_handler = sig_handler;
-
+    host = gethostbyname(argv[1]);
     sigaction(SIGINT, &act, NULL);
-
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
     /* vytvorime raw socket */
     sock = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
 
@@ -98,14 +99,50 @@ int main(int argc, char *argv[]) {
     my_sockaddr_in.sin_port = 0;
     memcpy(&my_sockaddr_in.sin_addr, host->h_addr, host->h_length);
 
+    for (int i = 0; i <= 5; i++) {
+        my_icmp_header.checksum = 0;
+        my_icmp_header.un.echo.sequence = i;
+        int sequence = my_icmp_header.un.echo.sequence;
 
-    my_icmp_header.checksum = 0;
-    my_icmp_header.un.echo.sequence = 0;
+        my_icmp_header.checksum = checksum((unsigned short *) & my_icmp_header, sizeof (my_icmp_header));
+        //pri checksumu s vyplnenym checksumem ma vyjit 0
+        if (checksum((unsigned short *) & my_icmp_header, sizeof (my_icmp_header)) != 0) {
+            printf("OMFG, spatny checksum");
+            return -1;
+        }
+        gettimeofday(&start, NULL);
+        sendto(sock, (char *) & my_icmp_header, sizeof (icmphdr), 0, (sockaddr *) & my_sockaddr_in, sizeof (sockaddr));
+        cout << "ping to " << argv[1] << ": " << endl;
+        //paket odeslan, cekame na odpoved
+        do {
+            FD_ZERO(&myset);
+            FD_SET(sock, &myset);
 
-    my_icmp_header.checksum = checksum((unsigned short *)&my_icmp_header, sizeof(my_icmp_header));
+            select(sock + 1, &myset, NULL, NULL, &timeout);
+            gettimeofday(&end, NULL);
+            if (FD_ISSET(sock, &myset)) {
+                size = sizeof (sockaddr_in);
+                recvfrom(sock, buffer, BUFSIZE, 0, (sockaddr *) & received_address, &size);
+            } else {
+                break;
+            }
+            //ted chytame ale vsechno, takze potreba vypsat jen kdyz to je nas hledany paket, popripade cekat dokud neprijde ten pravy
 
-    printf("odeslali sme data\n");
-    sendto(sock, (char *)&my_icmp_header, sizeof(icmphdr), 0, (sockaddr *)&my_sockaddr_in, sizeof(sockaddr));
+            ip_header = (iphdr *) buffer;
+            icmp_header = *(struct icmphdr*) (buffer + sizeof (ip_header));
+            cout << "typ (0 je fajn) : " << ntohs(icmp_header.type) << endl;
+            cout << "id (" << getpid() << " je fajn)" << ntohs(icmp_header.un.echo.id) << endl;
+            cout << "sequence (" << sequence << " je fajn)" << icmp_header.un.echo.sequence << endl << endl;
+        } while (!(ntohs(icmp_header.type) == ICMP_ECHOREPLY) && (ntohs(icmp_header.un.echo.sequence) == sequence));
+
+        if ((icmp_header.un.echo.id == getpid())
+                && (ntohs(icmp_header.type) == ICMP_ECHOREPLY)
+                && (icmp_header.un.echo.sequence == sequence)) {
+            cout << "RTT = " << 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << endl;
+        }
+
+
+    }
     /* zrusime socket */
     close(sock);
 
